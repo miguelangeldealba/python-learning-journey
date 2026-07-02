21ca9f51e6f597c477b4a80d1b48a9138788fcec
Miguel Angel de Alba y Pérez <miguelangel.trabajo.pr@gmail.com>
2026-07-02 18:29:33 +0200

    Delete B1 directory

--- B1/util/file_manager.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-class CSVFileManager:
-  def __init__(self,path: str):
-    self.path = path
-  def read(self) -> str:
-    return pd.read_csv(self.path)  
-  def write(self,dataFrame):
-    pass
